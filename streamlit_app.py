e25a92b13ab698b45256d8a212da12861694ebae
blue-ealge <takanakano101@gmail.com>
2023-12-28 12:15:35 +0900

    day2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 ## making streamlit app
 import streamlit as st
 
-st.write("hello world") 
+## making if else statement with streamlit showing different messages depending on if the button was pressed or not
+st.header('st.button')
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
